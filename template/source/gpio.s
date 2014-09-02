577106f17b71ea299faee39ce0f75a440e45b372
rad- <steven.t.seppala@gmail.com>
2014-09-02 15:11:29 -0600

    Updated to include gpio

--- template/source/gpio.s
+++ template/source/gpio.s
@@ -9,6 +9,18 @@ SetGpioFunction:
 	cmpls r1,#7			;only runs if previous is true
 	movhi pc,lr			;if pc > lr then pc = lr
 	nop					;HI C set and Z clear Higher (unsigned >)
-	push {lr}			
-	mov r2,r0
-	bl GetGpioAddress
+	push {lr}			;puts lr to top of the stack
+	mov r2,r0			;move right to left (r0 -> r2)
+	bl GetGpioAddress	
+	
+	
+functionLoop$:
+	cmp r2,#9			;compare r2 = 9
+	subhi r2,#10		;if r2>9, r2-10
+	addhi r0,#4			;r0 + 4
+	bhi functionLoop$	;This would be the same as GPIO Controller Address + 4 × (GPIO Pin Number ÷ 10).
+	add r2, r2,lsl #1	;One of the very useful features of the ARMv6 assembly code language is the ability to shift an argument before using it. 
+	nop					;In this case, I add r2 to the result of shifting the binary representation of r2 to the left by one place.
+	lsl r1,r2			;
+	str r1,[r0]			;
+	pop {pc}			;Only general purpose registers and pc can be popped.
