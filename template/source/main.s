023fdd68e695c844c1dc9a92b9cecf7052c214a8
rad- <steven.t.seppala@gmail.com>
2014-09-03 15:41:34 -0600

    Updated main

--- template/source/main.s
+++ template/source/main.s
@@ -5,5 +5,20 @@ _start:
 
 .section .text
 main:
-	mov sp,#0x8000	;setup stack pointer
-	
+	mov sp,#0x8000		;setup stack pointer
+	pinNum .req r0
+	pinFunc .req r1
+	mov pinNum,#16
+	mov pinFunc,#1
+	bl SetGpioFunction
+	.unreq pinNum
+	.unreq pinFunc
+	nop
+	pinNum .req r0
+	pinVal .req r1
+	mov pinNum,#16
+	mov pinVal,#0
+	bl SetGpio
+	.unreq pinNum
+	.unreq pinVal
+	nop
