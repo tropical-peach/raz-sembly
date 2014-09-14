2c9246cc24979f2e64828d351feee6223a24c6d8
rad- <steven.t.seppala@gmail.com>
2014-09-13 22:44:46 -0600

    Updated Makefile and main

--- template/source/main.s
+++ template/source/main.s
@@ -2,7 +2,7 @@
 	When linking to 'gpio' pass arguments for
 	'r0 <= address / pinNum'
 	'r1 <= function / value'
-/*
+*/
 
 .section .init
 .globl _start
@@ -11,29 +11,29 @@ _start:
 
 .section .text
 main:
-	mov sp,#0x8000		;setup stack pointer
-	pinNum .req r0		;alias these two pins
+	mov sp,#0x8000		@setup stack pointer
+	pinNum .req r0		@alias these two pins
 	pinFunc .req r1		
-	mov pinNum,#16		;select pin number 16
-	mov pinFunc,#1		;set pin function number 1
-	bl SetGpioFunction	;link to set function
-	.unreq pinNum		;unalias these two pins
+	mov pinNum,#16		@select pin number 16
+	mov pinFunc,#1		@set pin function number 1
+	bl SetGpioFunction	@link to set function
+	.unreq pinNum		@unalias these two pins
 	.unreq pinFunc
 	
 	
 loop$:
-	pinNum .req r0		;alias these two pins
-	pinVal .req r1		;
-	mov pinNum,#16		;set pin 16
-	mov pinVal,#0		;select function 0
+	pinNum .req r0		@alias these two pins
+	pinVal .req r1		@
+	mov pinNum,#16		@set pin 16
+	mov pinVal,#0		@select function 0
 	bl SetGpio
-	.unreq pinNum		;unalias
+	.unreq pinNum		@unalias
 	.unreq pinVal
 	nop					@nops
 	nop
 
-	decr .req r0		#'this is a pointless function'
-	mov decr,#0x3F0000	#'that kills time by looping'
+	decr .req r0		@'this is a pointless function'
+	mov decr,#0x3F0000	@'that kills time by looping'
 wait1$: 
 	sub decr,#1
 	teq decr,#0
@@ -59,6 +59,6 @@ wait2$:
 	bne wait2$
 	.unreq decr
 
-	# 'Repeat program'
+	@'Repeat program'
 	b loop$
 	nop
